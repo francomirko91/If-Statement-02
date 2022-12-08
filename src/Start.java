c81a488cd74cf429da31d975dac449d4b643affd
Mirko Franco <francomirko91@gmail.com>
2022-12-08 15:34:55 +0100

    If Statement 02

---
+++ src/Start.java
@@ -0,0 +1,10 @@
+public class Start {
+
+    public static void main(String[] args) {
+        String mySurname = "Frani";
+
+        String result = mySurname.contains("ni") ? "Your surname contains the sequence of letters 'ni'" : "Your surname doesn't contain the sequence of letters 'ni'";
+        System.out.println(result);
+
+    }
+}
